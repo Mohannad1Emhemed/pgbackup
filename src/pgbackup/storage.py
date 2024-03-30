dd357d4b9feb3a8f6d6d53bdcac79b68741d18bc
Mohannad Emhemed <mohaned1alrojpany@gmail.com>
2024-03-30 07:43:31 +0000

    mplemented S3 interactions

--- src/pgbackup/storage.py
+++ src/pgbackup/storage.py
@@ -2,3 +2,9 @@ def local(infile, outfile):
     outfile.write(infile.read())
     outfile.close()
     infile.close()
+
+def s3(client, infile, bucket, name):
+    client.upload_fileobj(infile, bucket, name)
+
+#with open(infile_path, 'rb') as infile:
+    #storage.s3(client, infile, 'python-db-backups-posgresql', infile.name)
